0b9f6ad9a75e387a9ee64a075b0ed959568f7bc3
Andres Urdaneta <aeum3893@gmail.com>
2019-12-08 17:01:17 -0500

    css modules implemented in the header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import headerStyles from './header.module.scss'
+
 const Header = () => (
   <>
     <header>
@@ -8,7 +10,7 @@ const Header = () => (
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className={headerStyles.link}>Home</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
