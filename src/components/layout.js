d148715f03c2a928fb4552de840b53afa4f493ee
Andres Urdaneta <aeum3893@gmail.com>
2019-12-08 16:40:48 -0500

    stylesheets folder, scss stylesheets, installed gatsby plugin to read scss files

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import React from "react"
 
 import Header from "./header"
 import Footer from "./footer"
-// import "./layout.css"
+import "../stylesheets/layout.scss"
 
 const Layout = ({ children }) => {
   return (
