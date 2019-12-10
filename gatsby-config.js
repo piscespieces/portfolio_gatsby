bce26a8c88c7757ae0164cf21e6f046d1ac0fa93
Andres Urdaneta <aeum3893@gmail.com>
2019-12-10 00:37:02 -0500

    added image in the markdown file, installed the plugins, declared the config options in the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,21 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -29,7 +44,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -5,4 +5,6 @@ date: "2019-12-08"
 
 This is just a test
 
+![Image](./image.png)
+
 ## And this is another MD test
