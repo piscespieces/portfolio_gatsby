227a57c1cedb00e04018a1f8c3684d350f902199
Andres Urdaneta <aeum3893@gmail.com>
2019-12-11 16:10:37 -0500

    centered sidebar along the image and the content. Also styled the footer. Implemented icons with react/icon FA, also gave icons a nice hove effect and linked them to the proper urls

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 
 import footerStyles from './footer.module.scss'
+import { FaLinkedin, FaGithub, FaInstagram, FaTwitter, FaSoundcloud } from 'react-icons/fa';
 
 const Footer = () => {
    const data = useStaticQuery(graphql`
@@ -16,7 +17,26 @@ const Footer = () => {
 
    return (
       <footer className={footerStyles.footer}>
-         <p>Created by {data.site.siteMetadata.author} © 2019</p>
+         <div className={footerStyles.contactIconsWrapper}>
+            <a href='www.linkedin.com/in/andres-urd' target='_blank'>
+               <FaLinkedin />
+            </a>
+            <a href='https://github.com/piscespieces' target='_blank'>
+               <FaGithub />
+            </a>
+            <a href='https://www.instagram.com/andreseurd/' target='_blank'>
+               <FaInstagram />
+            </a>
+            <a href='https://twitter.com/_andresur'>
+               <FaTwitter />
+            </a>
+            <a href='https://soundcloud.com/sadhanapisces'>
+               <FaSoundcloud />
+            </a>
+         </div>
+         <div className={footerStyles.design}>
+            <p>Designed by {data.site.siteMetadata.author} © 2019</p>
+         </div>
       </footer>
    )
 }
