d0a3939f79b6a256e25c6f3c05bc03fa512d95c8
Andres Urdaneta <aeum3893@gmail.com>
2019-12-16 17:05:36 -0500

    styled the links and headers

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaMailBulk } from 'react-icons/fa';
 
 import Layout from '../components/layout'
 import Head from '../components/head'
@@ -11,6 +12,12 @@ const ContactPage = () => (
          <main className={contactStyles.main}>
             <div className={contactStyles.contactWrapper}>
                <h1>I would love to hear from you</h1>
+               <h2><span><FaMailBulk /></span> aeum3893@gmail.com</h2>
+               <ul>
+                  <li><a href="https://linkedin.com/in/andres-urd">LinkedIn</a></li>
+                  <li><a href="https://github.com/piscespieces">Github</a></li>
+                  <li><a href="https://www.instagram.com/andreseurd/">Instagram</a></li>
+               </ul>
             </div>
          </main>
       </Layout>
